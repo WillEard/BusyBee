16f0f28855bb94b81e36cc0045d573290991fd9e
Will <earw1_19@uni.worc.ac.uk>
2023-06-02 21:57:39 +0100

    Implented DVLA Vehicle Enquiry API, registrations now work

--- BusyBee/RegCheck.php
+++ BusyBee/RegCheck.php
@@ -19,15 +19,18 @@
                     <form id="regForm" class="rounded" method="post" novalidate>
 
                         <div class="form-floating mb-3">
-                            <input type="text" class="form-control form-control-lg" id="validationUsername" name="uname" aria-describedby="usernameHelp" placeholder="Username" required>
+                            <input type="text" class="form-control form-control-lg" id="registration" name="registration" aria-describedby="usernameHelp" placeholder="Username" required>
                             <label for="username">Registration</label>
                         </div>
-                        
+                        <button class="btn btn-success btn-lg" type="submit" name="submit" id="btnGetReg" value="submit" data-toggle="tooltip" data-placement="top" title="Reg" hidden>Get Vehicle Information</button>
+
                     </form>
                 </div>
             </div>
         </div>
     </div>
+
+    
 
     <?php
         // DECLARE variable and SET to 'registration' id tag from form
@@ -57,116 +60,188 @@
               CURLOPT_POSTFIELDS =>"{\"registrationNumber\": \"$Registration\"}",
               // USE API Key to access API - API KEY IS HIGHLY SENSITIVE DO NOT EXPOSE
               CURLOPT_HTTPHEADER => array(
-                "x-api-key: ",
+                "x-api-key: ggHTfPh9Hz6DDGCn5kvk76LV4VTAOkG89oVUZY6C",
                 "Content-Type: application/json"
               ),
             ));
 
             // EXECUTE above commands and store inside variable
             $response = curl_exec($curl);
+
+            $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
             
             // CLOSE the statement
             curl_close($curl);	
-    
+
         // GET Error code returned
-        $errorCode = (http_response_code());
-        // IF error code is 404 - Registration entered is invalid
-        if ($errorCode == 404)
+        //var_dump(http_response_code());
+
+        // Bad Request
+        if ($httpcode === 400)
         {
             // DISPLAY error
-            echo "<div class='col-md-3 mx-auto alert alert-danger' role='alert'>Error: Invalid Registration!</div>";
+            echo '<h3 class="text-light">Error: Bad Request</h3>';
         }
-    ?>
+        // Forbidden
+        if ($httpcode === 403)
+        {
+            // DISPLAY error
+            echo '<h3 class="text-light">Error: Forbidden : Access Denied</h3>';
+        }
+        // Too many requests
+        if ($httpcode === 429)
+        {
+            // DISPLAY error
+            echo '<h3 class="text-light">Error: Too many requests, please wait.</h3>';
+        }
+        // Bad Gatway
+        if ($httpcode === 502)
+        {
+            // DISPLAY error
+            echo '<h3 class="text-light">Error: Bad Gateway</h3>';
+        }
+        // Gateway Timeout
+        if ($httpcode === 504)
+        {
+            // DISPLAY error
+            echo '<h3 class="text-light">Error: Gateway Timeout</h3>';
+        }
 
+        // Good
+        if ($httpcode === 200)
+        {
+            // DISPLAY error
+            echo '<h3 class="text-light text-center">Status: Success</h3>';
+        }
+
+        
+
+
+        
+
+        $string = $response;
+
+        $json = json_decode($string, true);
+
+        /* DEBUG - PRINT JSON TO SCREEN
+            print_r($json);
+        /*
+        /*JSON ELEMENTS
+            --registrationNumber,
+            --taxStatus,
+            taxDueDate,
+            --motStatus,
+            --make,
+            --yearOfManufacture,
+            --engineCapacity,
+            --co2Emissions,
+            --fuelType,
+            --markedForExport,
+            --colour,
+            --typeApproval,
+            --revenueWeight,
+            --dateOfLastV5CIssued,
+            --motExpiryDate,
+            --wheelplan,
+            --monthOfFirstRegistration
+        */
+    ?>
+    
     <div id="" class="container">
         <div class="row">
             <div class="col-md-4">
                 <div class="form-floating mb-3">
-                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" disabled>
+                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" value="<?php print_r ($json["registrationNumber"]); ?>" disabled>
                     <label class="fs-5" for="username">Registration</label>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="form-floating mb-3">
-                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" disabled>
+                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" value="<?php print_r ($json["make"]); ?>" disabled>
                     <label class="fs-5" for="username">Vehicle Make</label>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="form-floating mb-3">
-                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" disabled>
+                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" value="<?php print_r ($json["monthOfFirstRegistration"]); ?>" disabled>
                     <label class="fs-5" for="username">Date of Registration</label>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="form-floating mb-3">
-                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" disabled>
+                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" value="<?php print_r ($json["yearOfManufacture"]); ?>" disabled>
                     <label class="fs-5" for="username">Year of Manufacture</label>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="form-floating mb-3">
-                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" disabled>
+                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" value="<?php print_r ($json["engineCapacity"]); ?>" disabled>
                     <label class="fs-5" for="username">Cylinder Capacity</label>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="form-floating mb-3">
-                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" disabled>
+                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" value="<?php print_r ($json["co2Emissions"]); ?> g/km" disabled>
                     <label class="fs-5" for="username">C02 Emissions</label>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="form-floating mb-3">
-                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" disabled>
+                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" value="<?php print_r ($json["fuelType"]); ?>" disabled>
                     <label class="fs-5" for="username">Fuel Type</label>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="form-floating mb-3">
-                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" disabled>
-                    <label class="fs-5" for="username">Euro Status</label>
+                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" value="<?php print_r ($json["motStatus"]); ?>" disabled>
+                    <label class="fs-5" for="username">MOT Status</label>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="form-floating mb-3">
-                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" disabled>
-                    <label class="fs-5" for="username">Export Marker</label>
+                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" value="<?php print_r ($json["motExpiryDate"]); ?>" disabled>
+                    <label class="fs-5" for="username">MOT Expiry</label>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="form-floating mb-3">
-                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" disabled>
+                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" value="<?php print_r ($json["taxStatus"]); ?>" disabled>
                     <label class="fs-5" for="username">Vehicle Status</label>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="form-floating mb-3">
-                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" disabled>
+                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" value="<?php print_r ($json["taxDueDate"]); ?>" disabled>
+                    <label class="fs-5" for="username">Tax Due Date</label>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" value="<?php print_r ($json["colour"]); ?>" disabled>
                     <label class="fs-5" for="username">Vehicle Colour</label>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="form-floating mb-3">
-                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" disabled>
+                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" value="<?php print_r ($json["typeApproval"]); ?>" disabled>
                     <label class="fs-5" for="username">Vehicle Type</label>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="form-floating mb-3">
-                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" disabled>
+                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" value="<?php print_r ($json["wheelplan"]); ?>" disabled>
                     <label class="fs-5" for="username">Wheelplan</label>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="form-floating mb-3">
-                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" disabled>
+                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" value="<?php print_r ($json["revenueWeight"]); ?>" disabled>
                     <label class="fs-5" for="username">Revenue Weight</label>
                 </div>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-4 mx-auto">
                 <div class="form-floating mb-3">
-                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" disabled>
+                    <input type="text" class="form-control form-control-lg fw-bold pb-1" id="validationUsername" name="uname" aria-describedby="usernameHelp" value="<?php print_r ($json["dateOfLastV5CIssued"]); ?>" disabled>
                     <label class="fs-5" for="username">Date of last V5C</label>
                 </div>
             </div>
